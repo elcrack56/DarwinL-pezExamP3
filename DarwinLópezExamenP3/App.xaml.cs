18210d524c49e1226573860f5fbd4dd83803612f
laboratorio <laboratorio@UPOAULA10610>
2025-01-24 07:12:57 -0500

    Agregar archivos de proyecto.

---
+++ DarwinLópezExamenP3/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DarwinLópezExamenP3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
