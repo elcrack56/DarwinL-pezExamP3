a926de5e966a846011f7fb578b599c1241e959b5
elcrack56 <darwinsteven3@gmail.com>
2025-01-24 20:01:02 -0500

    eliminacion de algunas carpetas y modificacion de los modelos

---
+++ DarwinLópezExamenP3/Models/AeropuertoDL.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+
+namespace DarwinLópezExamenP3.Models
+{
+    public class Locacion
+    {
+        [JsonProperty("latitud")]
+        public double Latitud { get; set; }
+
+        [JsonProperty("longitud")]
+        public double Longitud { get; set; }
+    }
+
+    public class ContactoInformacion
+    {
+        [JsonProperty("telefono")]
+        public string Telefono { get; set; }
+
+        [JsonProperty("correo")]
+        public string Correo { get; set; }
+
+        [JsonProperty("Sitio web")]
+        public string SitioWeb { get; set; }
+    }
+
+    public class Aeropuertos
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("nombre")]
+        public string Nombre { get; set; }
+
+        [JsonProperty("codigo")]
+        public string Codigo { get; set; }
+
+        [JsonProperty("locacion")]
+        public Location Locacion { get; set; }
+
+        [JsonProperty("ciudad")]
+        public string Ciudad { get; set; }
+
+        [JsonProperty("pais")]
+        public string Pais { get; set; }
+
+        [JsonProperty("zona tiempal")]
+        public string Zona { get; set; }
+
+        [JsonProperty("contactoInformacion")]
+        public ContactoInformacion ContactoInformacion { get; set; }
+    }
+
+}
--- DarwinLópezExamenP3/Models/DLopezAeropuerto.cs
+++ DarwinLópezExamenP3/Models/DLopezAeropuerto.cs
@@ -11,10 +11,10 @@ namespace DarwinLópezExamenP3.Models
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
-        public string País { get; set; }
+        public string Pais { get; set; }
         public double Latitud { get; set; }
         public double Longitud { get; set; }
         public string Email { get; set; }
-        public string DLopez { get; set; }
+        public string Registradopor { get; set; } = "Dlopez";
     }
 }
